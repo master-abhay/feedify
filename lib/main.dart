451d259eda20a87bc5d0015d2b4fbe313a17bf7f
Abhay Kumar <159410007+master-abhay@users.noreply.github.com>
2025-03-26 00:48:53 +0530

    Revert "merge"
    This reverts commit 1b0272bdcc2f72b8516a092af1edf8d29db3b0fb.

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Feedify',
+      title: 'Flutter Demo',
       theme: lightTheme,
       navigatorKey: NavigationServices.instance.navigationStateKey,
       routes: NavigationServices.instance.routes,
